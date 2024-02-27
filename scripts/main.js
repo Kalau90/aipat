22a3fe307269b8541980380afccccb7bb8531a67
Kalau90 <45683444+Kalau90@users.noreply.github.com>
2024-02-27 13:29:10 +0100

    Popup working AAAAAAAAAAAAAND start text

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ class Cockpit{
         this.topEndpoint = "";
         this.leftEndpoint = "";
         this.rightEndpoint = "";
+        this.itemEndpoint = "";
 
         this.left_id = null;
         this.right_id = null;
@@ -15,12 +16,13 @@ class Cockpit{
     }
 
     configure(params){
-        const { left_endpoint, right_endpoint, top_endpoint, main_endpoint, left_queryparam, right_queryparam, top_queryparam } = params;
+        const { left_endpoint, right_endpoint, top_endpoint, main_endpoint, left_queryparam, right_queryparam, top_queryparam, item_endpoint } = params;
 
         this.leftEndpoint = left_endpoint;
         this.topEndpoint = top_endpoint;
         this.rightEndpoint = right_endpoint;
         this.mainEndpoint = main_endpoint;
+        this.itemEndpoint = item_endpoint;
 
         this.left_queryparam = left_queryparam;
         this.right_queryparam = right_queryparam;
@@ -40,8 +42,32 @@ class Cockpit{
         })
     }
 
-    openPopup(){
-        alert("HEJ")
+    async openPopup(id){
+        const item = await this.callAPI(this.itemEndpoint+id)
+        console.log(item.title)
+
+        const popup = document.getElementById("popup");
+
+        popup.innerHTML = "";
+
+        const case_heading = document.createElement("span")
+        case_heading.classList.add("heading")
+        case_heading.innerText = item.title;
+        popup.appendChild(case_heading);
+
+        const case_description = document.createElement("div")
+        case_description.classList.add("description")
+        case_description.innerText = item.description;
+        popup.appendChild(case_description)
+
+        const close = document.createElement("button")
+        close.innerText = "Back";
+        close.addEventListener("click", () => {
+            popup.classList.add("popup--hidden")
+        })
+        popup.appendChild(close)
+
+        popup.classList.remove("popup--hidden")
     }
 
     makeItems(items){
@@ -54,13 +80,42 @@ class Cockpit{
                 break;
             }
             const box = document.createElement("div");
-            box.innerHTML += "<div class='caseno'>"+item.name+"</div>";
-            box.innerHTML += "<span class='heading'>"+item.title+"</span>";
-            box.innerHTML += "<div class='manchet'>"+item.description+"</div>";
-            box.innerHTML += "<button onClick='console.log(\"hej\");'>Læs mere</button>"
+
+            const caseno = document.createElement("div")
+            caseno.classList.add("caseno")
+            caseno.innerText = item.name;
+            box.appendChild(caseno)
+
+            const heading = document.createElement("div")
+            heading.classList.add("heading")
+            heading.innerText = item.title;
+            box.appendChild(heading)
+            
+            const manchet = document.createElement("div")
+            manchet.classList.add("manchet")
+            manchet.innerText = item.description;
+            box.appendChild(manchet)
+            
+            const readmore = document.createElement("button")
+            readmore.addEventListener("click", () => {
+                this.openPopup(item.recid);
+            })
+            readmore.innerText = "Read more";
+            box.appendChild(readmore);
+
             box.classList.add("box")
             wrap.appendChild(box)
         }
+
+        
+        const popup = document.createElement("div");
+        popup.classList.add("popup");
+        popup.classList.add("popup--hidden");
+        popup.id="popup"
+
+        popup.innerHTML = "<h1>Dette er en popup</h1>"
+
+        wrap.appendChild(popup)
     }
 
     async showItems(){
@@ -125,11 +180,14 @@ class Cockpit{
         main_wrap.classList.add("items")
         main_wrap.id = "items";
 
-        const popup = document.createElement("div");
-        popup.classList.add("popup");
-        popup.innerHTML = "<h1>Dette er en popup</h1>"
-
-        main_wrap.appendChild(popup)
+        main_wrap.innerHTML = `<div style="width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center;"><div class="textnormal" style="text-align: center; font-size: 20px;"><span style="color: #0b0b0b; font-size: 20px; font-weight: bold;">Welcome to the AI-PAT system</span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;">The project is under development and will grow in cases and features.</span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;">Feedback and suggestions are useful</span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;"><br></span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;">SELECT TIMING, SITUATIONAL AND COMPLEXITY&nbsp;</span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;"><br></span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;">Best regards</span></div>
+        <div class="textnormal" style="text-align: center;"><span style="color: #0b0b0b;">/Mads</span></div></div>`
 
         /* FIT INTO HEAD AND MAIN */
         const header = document.createElement("div")
